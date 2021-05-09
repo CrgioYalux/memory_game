1a24b611b8736882a7daee09738150076f02b6cf
CrgioYalux <randaledomman1@gmail.com>
2021-05-09 04:58:51 -0300

    yarn added react-router-dom / created Background component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import Background from './components/Background';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<>
+			<Background />
+			<BrowserRouter>
+				<Redirect from="/" to="/home" />
+				<Switch>
+					<Route path="/home" render={Home} />
+					{/* <Route path="/projects" exact render={} /> */}
+				</Switch>
+			</BrowserRouter>
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/Background/index.js
@@ -0,0 +1,7 @@
+import './Background.css';
+
+const Background = () => {
+	return <div className="back"></div>;
+};
+
+export default Background;
