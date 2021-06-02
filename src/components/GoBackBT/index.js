23405821cde51decc730c833a9f780e5ca788b2a
CrgioYalux <randaledomman1@gmail.com>
2021-06-02 01:35:14 -0300

    the spinning gobackbt will be remembered as the legend that it would have been

--- src/components/GoBackBT/index.js
+++ src/components/GoBackBT/index.js
@@ -1,6 +1,7 @@
 import './GoBackBT.css';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import icon from '../../assets/arrow.svg';
 
 const GoBackBT = () => {
 	const location = useLocation();
@@ -8,7 +9,14 @@ const GoBackBT = () => {
 	return (
 		location.pathname !== '/' && (
 			<Link to="/" className="GoBackBT">
-				<p>{'<'}</p>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+				>
+					<path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+				</svg>
 			</Link>
 		)
 	);
